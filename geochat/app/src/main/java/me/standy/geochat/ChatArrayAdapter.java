869b01e4e897024921cc47470c6f7d33bd53994b
Andrew <karn9050@gmail.com>
2014-11-16 02:18:24 +0300

    Added ChatArrayAdapter

---
+++ geochat/app/src/main/java/me/standy/geochat/ChatArrayAdapter.java
@@ -0,0 +1,52 @@
+package me.standy.geochat;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by andrew on 16.11.14.
+ */
+public class ChatArrayAdapter extends ArrayAdapter {
+    private List<ChatMessage> chatMessageList = new ArrayList();
+
+    public void add(ChatMessage object) {
+        chatMessageList.add(object);
+        super.add(object);
+    }
+
+    public ChatArrayAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+    }
+
+    public int getCount() {
+        return this.chatMessageList.size();
+    }
+
+    public ChatMessage getItem(int index) {
+        return this.chatMessageList.get(index);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.single_message, parent, false);
+        }
+        LinearLayout singleMessageContainer = (LinearLayout) row.findViewById(R.id.singleMessageContainer);
+        ChatMessage chatMessageObj = getItem(position);
+        TextView chatText = (TextView) row.findViewById(R.id.singleMessage);
+        chatText.setText(chatMessageObj.message);
+        chatText.setBackgroundResource(chatMessageObj.isMine ? R.drawable.bubble_right : R.drawable.bubble_left);
+        singleMessageContainer.setGravity(chatMessageObj.isMine ? Gravity.LEFT : Gravity.RIGHT);
+        return row;
+    }
+}
