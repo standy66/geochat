d57d804f69da1ba1e05123660a65616c75dfb5e1
Penguin <fridaythethirteens@gmail.com>
2014-11-16 17:38:54 +0300

    log in activity changed

--- geochat/app/src/main/java/me/standy/geochat/LoginActivity.java
+++ geochat/app/src/main/java/me/standy/geochat/LoginActivity.java
@@ -1,20 +1,82 @@
 package me.standy.geochat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKSdkListener;
+import com.vk.sdk.VKUIHelper;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.dialogs.VKCaptchaDialog;
 
 
 public class LoginActivity extends Activity {
     private Button logInButton;
+    private final static String APP_ID = "4636866";
+    private final static String TOKEN_NAME = "vk_access_token";
+    private final static String[] myScope = {VKScope.AUDIO};
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        VKUIHelper.onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        VKUIHelper.onResume(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VKUIHelper.onDestroy(this);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         logInButton = (Button) findViewById(R.id.logInButton);
-        logInButton.setOnClickListener();
+        logInButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                VKSdk.initialize(new VKSdkListener() {
+                    @Override
+                    public void onCaptchaError(VKError captchaError) {
+                        new VKCaptchaDialog(captchaError).show();
+                    }
+
+                    @Override
+                    public void onTokenExpired(VKAccessToken expiredToken) {
+                        VKSdk.authorize(myScope);
+                    }
+
+
+                    @Override
+                    public void onReceiveNewToken(VKAccessToken newToken) {
+                        newToken.saveTokenToSharedPreferences(LoginActivity.this,TOKEN_NAME);
+                    }
+
+                    @Override
+                    public void onAccessDenied(VKError authorizationError) {
+                        new AlertDialog.Builder(LoginActivity.this).setMessage(authorizationError.errorMessage).show();
+                    }
+                }, APP_ID,VKAccessToken.tokenFromSharedPreferences(LoginActivity.this,TOKEN_NAME));
+
+            }
+        });
+        VKSdk.authorize(myScope, true, false);
     }
 
 
