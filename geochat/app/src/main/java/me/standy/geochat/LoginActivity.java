c6b78e1f8b2549d21c5c3393c3e28e937aad597b
Penguin <fridaythethirteens@gmail.com>
2014-11-16 12:55:24 +0300

    log in button

--- geochat/app/src/main/java/me/standy/geochat/LoginActivity.java
+++ geochat/app/src/main/java/me/standy/geochat/LoginActivity.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class LoginActivity extends Activity {
-
+    private Button logInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        logInButton = (Button) findViewById(R.id.logInButton);
+        logInButton.setOnClickListener();
     }
 
 
